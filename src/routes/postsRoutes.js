6f267800c31ce968b7040a5fc6af6dbbb96359b9
LaisZagati <laisviana97@gmail.com>
2024-11-22 21:37:24 +0000

    Image added

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,18 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsControllers.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsControllers.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads"})
+
 
 // Define as rotas usando o objeto Express app
 const routes = (app) => {
@@ -11,6 +24,7 @@ const routes = (app) => {
   
     // Rota para criar um novo post
     app.post("/posts", postarNovoPost); // Chama a função controladora para criação de posts
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
